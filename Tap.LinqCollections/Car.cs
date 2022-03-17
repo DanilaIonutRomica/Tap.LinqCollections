e734ec0589fa102966fb0781bf6d7b893171d3a1
danilaionutromica <danilaionut8@gmail.com>
2022-03-17 16:10:21 +0200

    sdftg

---
+++ Tap.LinqCollections/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tap.LinqCollections
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        public bool IsElectric { get; set; }
+        public int MaxSpeedKhm { get; set; }
+        public int AutonomyKm { get; set; }
+    }
+}
